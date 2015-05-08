255f65e537a7cf897982ad27c01b6e34a90b4fef
Gerald Spencer <ger.spencer3@gmail.comm>
2015-05-08 11:47:26 -0700

    Pub/Sub server

---
+++ pubSubClient.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#   Dreamland output client
+#   Connects SUB socket to tcp://localhost:5556
+#   Collects various effect outputs
+
+import sys
+import zmq
+
+#  Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print("Connecting to Dreamland server…")
+socket.connect("tcp://localhost:5556")
+
+# Subscribe to object of Dreamland (carosuel, bench, lightpost, ect...)
+dreamLandObject = sys.argv[1] if len(sys.argv) > 1 else "oops"
+socket.setsockopt_string(zmq.SUBSCRIBE, dreamLandObject)
+print("Subscribed to %s" % dreamLandObject)
+
+while True:
+	string = socket.recv_string()
+	subcribedObject, number = string.split()
+	print("Recieved: %s" % number)
---
+++ pubSubServer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+#   Dreamland output server
+#   Binds PUB socket to tcp://*:5556
+#   Publishes effect outputs to network
+#
+
+import zmq
+import time
+
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:5556")
+
+while True:
+	dreamLandObject = "bench1"
+
+	for number in range(10):
+		socket.send_string("%s %i " % (dreamLandObject, number))
+		time.sleep(1)
