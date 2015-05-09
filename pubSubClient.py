6b51ae6bdf18ba78513714dd2225c625b29cf7c5
Gerald Spencer <ger.spencer3@gmail.comm>
2015-05-08 20:37:04 -0700

    Error message

--- pubSubClient.py
+++ pubSubClient.py
@@ -15,7 +15,7 @@ print("Connecting to Dreamland server…")
 socket.connect("tcp://localhost:5556")
 
 # Subscribe to object of Dreamland (carosuel, bench, lightpost, ect...)
-dreamLandObject = sys.argv[1] if len(sys.argv) > 1 else "oops"
+dreamLandObject = sys.argv[1] if len(sys.argv) > 1 else "You forgot to add the object!"
 socket.setsockopt_string(zmq.SUBSCRIBE, dreamLandObject)
 print("Subscribed to %s" % dreamLandObject)
 
