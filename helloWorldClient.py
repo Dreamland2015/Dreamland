ebda093a8926643dcdd989bbecbf1f5039cfa707
Gerald Spencer <ger.spencer3@gmail.comm>
2015-05-13 01:01:10 -0700

    almost

--- helloWorldClient.py
+++ helloWorldClient.py
@@ -15,7 +15,7 @@ context = zmq.Context()
 #  Socket to talk to server
 print("Connecting to hello world server…")
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+socket.connect("tcp://192.168.2.3:5555")
 
 #  Do 10 requests, waiting each time for a response
 while True:
