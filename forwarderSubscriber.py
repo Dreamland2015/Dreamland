2773212bc7eab322d1e5dfac3f8fa6f265877f35
Gerald Spencer <ger.spencer3@gmail.comm>
2015-05-13 00:06:37 -0700

    Forwarder device and multiprocess, pub and a sub on one box

--- forwarderSubscriber.py
+++ forwarderSubscriber.py
@@ -2,14 +2,18 @@ import sys
 import zmq
 
 port = "5560"
+dreamlandObject = b"9"
+
 # Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 print("Collecting updates from Dreamland servers...")
+
 socket.connect("tcp://localhost:%s" % port)
-topicfilter = b"9"
-socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
-for update_nbr in range(2):
-    string = socket.recv()
-    topic, messagedata = string.split()
-    print(topic, messagedata)
+
+socket.setsockopt(zmq.SUBSCRIBE, dreamlandObject)
+
+while True:
+	recievedString = socket.recv()
+	subscribed, command = recievedString.split()
+	print(command)
