7be13375193c55ea47c094f38d8b319970983fb6
Gerald Spencer <ger.spencer3@gmail.comm>
2015-05-17 20:56:29 -0700

    print

--- Archive/forwarderPublisher.py
+++ Archive/forwarderPublisher.py
@@ -9,7 +9,7 @@ socket.connect("tcp://localhost:%s" % port)
 publisher_id = random.randrange(0, 9999)
 while True:
     topic = random.randrange(1, 10)
-    messagedata = "server#" + publisher_id
-    print "%s %s" % (topic, messagedata)
+    messagedata = "server#" + str(publisher_id)
+    print("%s %s" % (topic, messagedata))
     socket.send_string("%d %s" % (topic, messagedata))
     time.sleep(1)
