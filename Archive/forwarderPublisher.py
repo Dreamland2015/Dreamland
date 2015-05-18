0954eaec65fa00c94e9c6d49564ee7eceaa0490d
Gerald Spencer <ger.spencer3@gmail.comm>
2015-05-17 20:49:50 -0700

    IT WORKS

---
+++ Archive/forwarderPublisher.py
@@ -0,0 +1,15 @@
+import zmq
+import random
+import time
+
+port = "2222"
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.connect("tcp://localhost:%s" % port)
+publisher_id = random.randrange(0, 9999)
+while True:
+    topic = random.randrange(1, 10)
+    messagedata = "server#" + publisher_id
+    print "%s %s" % (topic, messagedata)
+    socket.send("%d %s" % (topic, messagedata))
+    time.sleep(1)
