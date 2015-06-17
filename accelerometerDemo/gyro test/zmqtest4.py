f0750c5c6a7f90c4468999fc8caeab44e313d30b
Marc H <mphertlein@gmail.com>
2015-06-16 23:25:56 -0700

    ZMQ pub/sub tests
    Checking whether published messages get queued up if subscriber doesn't
    read them (answer: yes). Hm. Don't want that if we're sending
    accelerometer/gyro data - that needs to be fresh, not old queued data.

--- accelerometerDemo/gyro test/zmqtest4.py
+++ accelerometerDemo/gyro test/zmqtest4.py
@@ -1,24 +1,27 @@
-# Demo program for zmq
-# 
-# Tests zmq messaging between different processes.
-# This works, but is only a genera demo. We need something more specific.
-# Might need nonblocking socket calls.
-#
-# Starts two servers (a push server, and a publish server) using 
-# multiprocessing. Then starts a client which listens to messages
-# from the servers.
-# Normally of course we would run these on different machines connected via
-# ethernet, and this code allows the machines to communicate via zmq. Here
-# we're running them from the same file for test purposes.
-#
-# Code copied from 
-# https://learning-0mq-with-pyzmq.readthedocs.org/en/latest/pyzmq/multisocket/zmqpoller.html
-# and modified so it works on python 3
-#
-# One problem with sending string messages is that there is no standard
-# way to encode the strings (bytes? characters? unicode characters?)
-# before sending them.
-# see: https://zeromq.github.io/pyzmq/unicode.html
+# -*- coding: utf-8 -*-
+"""
+ Demo program for zmq
+ 
+ Tests zmq messaging between different processes.
+ This works, but is only a genera demo. We need something more specific.
+ Might need nonblocking socket calls.
+
+ Starts two servers (a push server, and a publish server) using 
+ multiprocessing. Then starts a client which listens to messages
+ from the servers.
+ Normally of course we would run these on different machines connected via
+ ethernet, and this code allows the machines to communicate via zmq. Here
+ we're running them from the same file for test purposes.
+
+ Code copied from 
+ https://learning-0mq-with-pyzmq.readthedocs.org/en/latest/pyzmq/multisocket/zmqpoller.html
+ and modified so it works on python 3
+
+ One problem with sending string messages is that there is no standard
+ way to encode the strings (bytes? characters? unicode characters?)
+ before sending them.
+ see: https://zeromq.github.io/pyzmq/unicode.html
+"""
 
 import zmq
 import time
