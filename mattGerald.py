d6bd92d3bf474aeace648eea802c14b00efbea1f
Gerald Spencer <ger.spencer3@gmail.comm>
2015-06-17 05:44:06 -0700

    parallel ssh scripting

---
+++ mattGerald.py
@@ -0,0 +1,85 @@
+import paramiko
+import sys
+import time
+from threading import Thread
+
+login = "pi"
+password = "raspberry"
+
+config = [
+    {"hostname": "pi.local", "structureName": "carousel"},
+    {"hostname": "pi1.local", "structureName": "bench1"},
+    {"hostname": "pi2.local", "structureName": "bench2"}
+]
+
+
+def doSomethingHere():
+    pass
+
+
+def writeConfigFile():
+    f = open("config.py", "w")
+
+    var = {"structureName": "yourMom",
+           "hotname": "pi@pi.local",
+           "serverIP": "8.8.8.8"}
+
+    for k, v in var.items():
+        f.write('{} = "{}"\n'.format(k, v))
+
+
+class SSHConnection():
+    def __init__(self, hostname, structureName):
+        self.hostname = hostname
+        self.structureName = structureName
+        self.connectToSSH()
+
+    def connectToSSH(self):
+        for x in range(10):
+            try:
+                self.ssh = paramiko.SSHClient()
+                self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                self.ssh.connect(self.hostname, username=login, password=password)
+                print ("Connected to " + self.structureName)
+                break
+            except paramiko.AuthenticationException:
+                print ("Authentication failed when connecting to " + self.structureName)
+                sys.exit(1)
+            except:
+                print ("Could not SSH to " + self.structureName + ", waiting for it to start")
+                time.sleep(1)
+        return
+
+        # If no connection in the allowed time.
+        raise RuntimeError("Could not connect to " + self.structureName + ". Giving up")
+
+    def runCommand(self):
+        pass
+
+
+class MultiSSH:
+    def __init__(self, listOfStructures):
+        self.listOfStructures = listOfStructures
+        self.sshConnections = []
+        for structure in listOfStructures:
+            self.sshConnections.append(SSHConnection(structure["hostname"], structure["structureName"]))
+            # self.sshConnections.append([SSHConnection(structure["hostname"], strucuture["structureName"]) for hostname, structureName in structure.items()])
+
+    def runOnAll(self, commandToRun):
+        running = []
+        for connection in self.sshConnections:
+            # Arbitraily complicated stuff
+            th = Thread(target=connection.runCommand, args=commandToRun)
+            running.append(th)
+
+        for r in running:
+            r.join()
+
+    def runOnGroup(self, groupDesc, commandToRun):
+        pass
+
+    def runMultipleCommandsOnAll(self, commandList):
+        pass
+
+    def setupConfigFile(self, stuffIWantInThere):
+        self.runOnAll("echo SOME TERRIBLE STRING I MAKE IN CODE > config.py")
--- sshTest.py
+++ sshTest.py
@@ -21,19 +21,19 @@ class Dreamlandia():
         for index, dreamlandObject in enumerate(dreamlandObjects):
             self.structureName = dreamlandObject['name']
             self.host = dreamlandObject['hostname']
-            print(self.host + ',' + self.structureName)
-            pool = mp.Pool(processes=12)
-            results = [pool.apply_async(self.connectToServer, args=(dreamlandObject,)) for dreamlandObject in config]
+            pool = mp.Pool(processes=len(config))
+            results = [pool.apply_async(self.connectToServer, args=()) for dreamlandObject in config]
             output = [p.get() for p in results]
 
     def connectToServer(self):
         i = 1
         while True:
             try:
-                ssh = paramiko.SSHClient()
-                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                ssh.connect(self.host, username=login, password=password)
+                self.ssh = paramiko.SSHClient()
+                self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                self.ssh.connect(self.host, username=login, password=password)
                 print ("Connected to " + self.structureName)
+                self.startStructure()
                 break
             except paramiko.AuthenticationException:
                 print ("Authentication failed when connecting to " + self.structureName)
@@ -41,14 +41,15 @@ class Dreamlandia():
             except:
                 print ("Could not SSH to " + self.structureName + ", waiting for it to start")
                 i += 1
-                time.sleep(.5)
+                time.sleep(1)
 
             # If we could not connect within time limit
             if i == 30:
                 print ("Could not connect to " + self.structureName + ". Giving up")
                 sys.exit(1)
 
-    # stdin, stdout, stderr = ssh.exec_command("python3 /home/pi/repo/Dreamland/DreamlandDemo/python/dreamlandStructure.py bench1 10.0.1.165")
+    def startStructure(self):
+        stdin, stdout, stderr = self.ssh.exec_command("python3 /home/pi/repo/Dreamland/DreamlandDemo/python/dreamlandStructure.py" + self.structureName + ' ' + serverIp)
 
 
 if __name__ == '__main__':
