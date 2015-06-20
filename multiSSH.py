d2d43fd10be9ca711e0f285998af3e04094743d3
Gerald Spencer <ger.spencer3@gmail.comm>
2015-06-20 09:10:03 -0700

    added echo class

--- multiSSH.py
+++ multiSSH.py
@@ -92,3 +92,25 @@ class MultiSSH:
 
     def setupConfigFile(self, stuffIWantInThere):
         self.runOnAll("echo SOME TERRIBLE STRING I MAKE IN CODE > config.py")
+
+
+class echo():
+    def __init__(self, fileName):
+        self.fileName = fileName
+
+    def writeConfig(self, configDict):
+        keys = list(configDict.keys())
+
+        configToWrite = []
+
+        for index, key in enumerate(keys):
+            if index == 0:
+                string = """echo {"'%s'": "'%s'",  >> %s""" % (key, configDict[key], self.fileName)
+            elif index == len(keys) - 1:
+                string = """echo "'%s'": "'%s'"}  > %s""" % (key, configDict[key], self.fileName)
+            else:
+                string = """echo "'%s'": "'%s'",  > %s""" % (key, configDict[key], self.fileName)
+
+            configToWrite.append(string)
+
+        return configToWrite
