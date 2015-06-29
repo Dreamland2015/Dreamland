e235cf9863f173b6d6cc475ec31397f60b1d229a
Gerald Spencer <ger.spencer3@gmail.comm>
2015-06-28 23:42:47 -0700

    initial commit of the file that will be used to setup and manage Dreamland

---
+++ SetupDreamland.py
@@ -0,0 +1,10 @@
+from multiSSH import MultiSSH
+
+
+serverIp = "10.0.1.165"
+
+config = {
+    "Carousel": {"hostname": "pi.local", "serverIp": serverIp},
+    "Bench": {"hostname": "pi.local", "serverIp": serverIp},
+    "Bench1": {"hostname": "pi.local", "serverIp": serverIp}
+}
