75e210b56134c546352b21156bc68628e22d6fb8
Gerald Spencer <ger.spencer3@gmail.comm>
2015-06-28 19:17:14 -0700

    script to launch RPI python3 script on boot

---
+++ launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd ~/repo/Dreamland
+sudo python3 dreamlandStructure.py
+cd ~
