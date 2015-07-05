ac771d73a6d0187626242bdbfcc231459ab6aaf2
Gerald Spencer <ger.spencer3@gmail.comm>
2015-07-05 15:26:30 -0700

    sunday afternoon check in. testConfig.py to test the structureConfig dict. Removed some files and such.

---
+++ testConfig.py
@@ -0,0 +1,5 @@
+structureConfig = {'structureName': 'carousel',
+                   'hostname': 'pi.local',
+                   'serverIp': '10.0.1.165',
+                   'outputPins': '[3, 4]',
+                   'inputPins': '[1, 2]'}
