0e72dc8261e407a64f62447255f63107a873ba0b
Gerald Spencer <ger.spencer3@gmail.comm>
2015-07-19 17:00:44 -0700

    formating

--- helloWorldSub.py
+++ helloWorldSub.py
@@ -1,7 +1,5 @@
 from structureConfig import structureConfig
 import pubSubDreamland as psdl
-# import gpioDreamland as ioDL
-import fakeGPIO as ioDL
 
 structureName = structureConfig["structureName"]
 serverIp = structureConfig["serverIp"]
@@ -11,12 +9,6 @@ send_port = '5559'
 
 sub = psdl.Subscriber(serverIp, recv_port, structureName, isServer=False)
 pub = psdl.Publisher(serverIp, send_port, structureName, isServer=False)
-
-outputPins = ioDL.stringToList(structureConfig['outputPins'])
-print(outputPins)
-
-ioDL.setMode()
-outputPins = [ioDL.Poofer(pin) for pin in outputPins]
 
 while True:
     command = sub.recvMessage()
