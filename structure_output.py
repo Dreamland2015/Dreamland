da75b88bf02f1b24eb030b84011ec5467ca066bc
Samson Yeung <samson.yeung@oracle.com>
2015-07-22 12:48:26 -0700

    structure_output.py: Use new util functions.

--- structure_output.py
+++ structure_output.py
@@ -12,15 +12,11 @@ args = docopt.docopt(__doc__)
 import util
 import GPIO
 
-config = util.read_config()
-sub = util.SubClient(config['master'], config['topic'])
+config = util.DreamlandConfig(args['<config_file>'])
+sub = util.SubClient(config.master(), config.topic())
+
+outputs = GPIO.GPIO_outputs(config.output())
 
 while True:
     action, level = sub.recv()
-    try:
-        GPIO.set(config[action], level)
-    except KeyError:
-        import traceback
-        traceback.print_exc()
-        err_msg = "Error on %s trying to set %s to %s" % (config['topic'], action, level)
-        pub = util.PubClient(config['master'], 'error').send(err_msg)
+    outputs.output(action, int(level))
