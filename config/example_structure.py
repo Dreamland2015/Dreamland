291ef20cc20bded7541e9128229d6d2f4b034e5b
Samson Yeung <samson.yeung@oracle.com>
2015-07-27 22:23:08 -0700

    config/example_structure: Add 2 example outputs.

--- config/example_structure.py
+++ config/example_structure.py
@@ -1,5 +1,7 @@
 config = {'topic': 'carousel',
           'input': {'button': {'pin': 7}},
-          'output': {'flame1': {'pin': 11}},
+          'output': {'flame1': {'pin': 11},
+                     'flame2': {'pin': 13},
+          },
           'hostname': 'pi7.local',
           'master': '192.168.2.49'}
