f7c0dfd46209c1ae4a9dab23239e465d0941c5d1
Samson Yeung <samson.yeung@oracle.com>
2015-07-28 09:48:43 -0700

    structure_output: Don't die on bad/unknown messages.

--- structure_output.py
+++ structure_output.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import traceback
 
 import util
 import GPIO
@@ -15,6 +16,12 @@ sub = util.SubClient(config.master(), config.topic())
 
 outputs = GPIO.GPIO_outputs(config.output())
 
+print(config.config)
+
 while True:
     action, level = sub.recv()
-    outputs.output(action, int(level))
+    print("action", action, level)
+    try:
+        outputs.output(action, int(level))
+    except KeyError:
+        traceback.print_exc()
