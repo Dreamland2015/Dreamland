ac09ba5c13d6157a46eb9a504f321dd470e1fb2e
Samson Yeung <samson.yeung@oracle.com>
2015-07-27 22:17:43 -0700

    Update example config.

--- config/example_structure.py
+++ config/example_structure.py
@@ -1,8 +1,5 @@
-master = 'localhost'
-topic = 'carousel'
-
-output = {'center':{'pin':1},
-          'side': {'pin': 4},
-}
-
-input = {'name': 'button', 'pin': 4}
+config = {'topic': 'carousel',
+          'input': {'button': {'pin': 7}},
+          'output': {'flame1': {'pin': 11}},
+          'hostname': 'pi7.local',
+          'master': '192.168.2.49'}
