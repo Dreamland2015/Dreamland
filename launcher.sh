133a2f0911f19eecd6ddbe5a7b10bb90c94b09e2
Samson Yeung <samson.yeung@oracle.com>
2015-08-02 15:43:05 -0700

    Drop deprecated code.

--- launcher.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-sudo python3 /home/pi/repo/Dreamland/dreamlandStructure.py
--- testConfig.py
+++
@@ -1,5 +0,0 @@
-structureConfig = {'structureName': 'Carousel',
-                   'hostname': 'pi2.local',
-                   'serverIp': 'localhost',
-                   'outputPins': '7, 11, 13, 15',
-                   'inputPins': '[1, 2]'}
