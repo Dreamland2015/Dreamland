a2ddd1ed05c114f3cdc45e6f4ed13ab06aec2758
Marc <mphertlein@gmail.com>
2015-08-10 23:10:59 -0700

    gear sensor image added

--- sensorDemo/gear/gearsensor_setup_pins.py
+++ sensorDemo/gear/gearsensor_setup_pins.py
@@ -6,8 +6,11 @@ gearsensor3_pin = 22
 
 
 if __name__ == "__main__":
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(gearsensor1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(gearsensor2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(gearsensor3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(gearsensor1b_pin, GPIO.IN)
+    GPIO.setup(gearsensor2b_pin, GPIO.IN)
+    GPIO.setup(gearsensor3b_pin, GPIO.IN)
+    GPIO.setup(gearsensor1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(gearsensor2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(gearsensor3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
