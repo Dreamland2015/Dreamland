0583b3e2abcbb771625ef9e8a9b441b1ec1acb5a
geethree <ger.spencer3@gmail.com>
2015-08-14 21:21:23 -0700

    config fixing

--- SetupDreamland.py
+++ SetupDreamland.py
@@ -7,58 +7,39 @@ import multiSSH
 serverIp = util.get_default_ip()
 
 configs = [
-    { 'topic': 'carousel-top',
-      'hostname': "pi15.local",
-      'input': {'button':{'pin':1}},
-      'output': {
-          'flame1':{'pin':6},
-          'flame2':{'pin':13},
-          'flame3':{'pin':19},
-          'center_flame':{'pin':26},
-          },
-      'master': serverIp,
+    {
+        'topic': 'carousel-top',
+        'hostname': "pi15.local",
+        'input': {'button': {'pin': 1}},
+        'output': {
+            'flame1': {'pin': 6},
+            'flame2': {'pin': 13},
+            'flame3': {'pin': 19},
+            'center_flame': {'pin': 26}},
+        'master': serverIp,
     },
     {
         'topic': 'carousel',
         'hostname': "pi1.local",
-        'input': {'button':{'pin':7}},
-        'output': { },
-            #'flame1':{'pin':7},
-            #'flame2':{'pin':11},
-            #'flame3':{'pin':13},
-            #'center_flame':{'pin':15},
-            #},
+        'input': {'button': {'pin': 7}},
         'master': serverIp,
     },
     {
         'topic': 'carousel',
         'hostname': "samsonpi.local",
-        'input': {'button':{'pin':7}},
-        'output': { },
-            #'flame1':{'pin':7},
-            #'flame2':{'pin':11},
-            #'flame3':{'pin':13},
-            #'center_flame':{'pin':15},
-            #},
+        'input': {'button': {'pin': 7}},
         'master': serverIp,
     },
     {
         'topic': 'lantern1',
         'hostname': "pi8.local",
-        'input': {'button':{'pin':7}},
-        'output': { },
-            #'flame1':{'pin':7},
-            #'flame2':{'pin':11},
-            #'flame3':{'pin':13},
-            #'center_flame':{'pin':15},
-            #},
+        'input': {'button': {'pin': 7}},
         'master': serverIp,
     },
     {
         'topic': 'carousel-top',
         'hostname': "pi15.local",
-        'input': {'button':{'pin':7}},
-        'output': { },
+        'input': {'button': {'pin': 7}},
         'master': serverIp,
     },
     {
@@ -71,9 +52,16 @@ configs = [
         'hostname': "pi9.local",
         'master': serverIp,
     },
+    {
+        'topic': 'lantern3',
+        'hostname': "pi4.local",
+        'input': {'button': {'pin': 7}},
+        'output': {},
+        'master': serverIp,
+    }
 ]
 
 s = multiSSH.MultiDreamandPi([configs[-1]], debug=True)
-s.do_full_setup()
-#s.do_partial_setup()
+# s.do_full_setup()
+s.do_partial_setup()
 print('Pi is now fully setup')
